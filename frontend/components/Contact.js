f22616c7adb58287fd8325b2c0efeaf9326d64bc
kunukulavanya03 <kunukulavanya03@gmail.com>
2025-11-28 16:04:11 +0530

    🚀 Initial commit - Generated with FigmaToFullApp

---
+++ frontend/components/Contact.js
@@ -0,0 +1,10 @@
+import Form from './Form';
+
+export default function Contact() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">Get in touch</h1>
+      <Form />
+    </div>
+  );
+}
